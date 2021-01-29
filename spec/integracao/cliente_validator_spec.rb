35330f33936c7a0ab835a998db1527101b557b62
Joacir Oliveira Santos Júnior <jr.joacir@gmail.com>
2021-01-29 14:13:43 -0300

    Refatorado testes da classe ClienteValidator

---
+++ spec/integracao/cliente_validator_spec.rb
@@ -0,0 +1,5 @@
+require_relative '../rspec_setup'
+
+RSpec.describe ClienteValidator do
+  puts 'Não há teste de integração da classe ClienteValidator, pois não há chamada externa'
+end
--- spec/unitario/cliente_validator_spec.rb
+++ spec/unitario/cliente_validator_spec.rb
@@ -1,10 +1,4 @@
-require 'simplecov'
-SimpleCov.start
-require_relative '../app/model/cliente.rb'
-require_relative '../app/serializers/cliente_serializer.rb'
-require_relative '../app/repositories/cliente_repository.rb'
-require_relative '../app/validators/cliente_validator.rb'
-require_relative '../app/controllers/cliente_controller.rb'
+require_relative '../rspec_setup'
 
 RSpec.describe ClienteValidator do
   subject { ClienteValidator.criacao!(parametros) }
@@ -13,7 +7,7 @@ RSpec.describe ClienteValidator do
     context 'e código do cliente não informado' do
       let(:parametros) { { nome: 'Nome do cliente' } }
 
-      it 'dispara RuntimeError' do
+      it 'dispara erro do tipo RuntimeError e sua mensagem' do
         expect{subject}.to raise_error(RuntimeError, 'Código do cliente não informado')
       end
     end
@@ -21,33 +15,33 @@ RSpec.describe ClienteValidator do
     context 'e nome do cliente não informado' do
       let(:parametros) { { id: 1 } }
 
-      it 'dispara RuntimeError' do
+      it 'dispara erro do tipo RuntimeError e sua mensagem' do
         expect{subject}.to raise_error(RuntimeError, 'Nome do cliente não informado')
       end
     end
 
-    context 'e código do cliente não é um Inteiro' do
+    context 'e código do cliente não é do tipo Integer' do
       let(:parametros) { { id: '1', nome: 'Nome do cliente' } }
 
-      it 'dispara RuntimeError' do
+      it 'dispara erro do tipo RuntimeError e sua mensagem' do
         expect{subject}.to raise_error(RuntimeError, 'Código do cliente não é um inteiro')
       end
     end
 
-    context 'e nome do cliente não é um Texto' do
+    context 'e nome do cliente não é do tipo String' do
       let(:parametros) { { id: 1, nome: 123 } }
 
-      it 'dispara RuntimeError' do
+      it 'dispara erro do tipo RuntimeError e sua mensagem' do
         expect{subject}.to raise_error(RuntimeError, 'Código do cliente não é um texto')
       end
     end
   end
 
   context 'Quando não há erros na ṽalidação' do
-    let(:parametros) { { id: 1, nome: 'Nome do cliente', data_nascimento: nil } }
+    let(:parametros) { { id: 1, nome: 'Nome do cliente', data_nascimento: nil, campo_nao_autorizado: 'não autorizo' } }
     let(:retorno_esperado) { { id: parametros[:id], nome: parametros[:nome], data_nascimento: parametros[:data_nascimento] } }
 
-    it 'retorna hash com os dados válidos' do
+    it 'retorna hash com os dados e valores válidos' do
       expect(subject).to eq retorno_esperado
     end
   end
